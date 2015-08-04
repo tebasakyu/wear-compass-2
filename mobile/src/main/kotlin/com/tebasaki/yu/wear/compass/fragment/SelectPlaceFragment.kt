b6ee0c70e2b2eaf05d693a0da5c7eef657718382
s-makotoy <tebasakyu@yahoo.co.jp>
2015-08-04 22:15:34 +0900

    wip add place auto complete

--- mobile/src/main/kotlin/com/tebasaki/yu/wear/compass/fragment/SelectPlaceFragment.kt
+++ mobile/src/main/kotlin/com/tebasaki/yu/wear/compass/fragment/SelectPlaceFragment.kt
@@ -4,13 +4,17 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.TextView
 import butterknife.bindView
+import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.location.places.Place
+import com.google.android.gms.location.places.Places
 import com.google.android.gms.location.places.ui.PlacePicker
 import com.tebasaki.yu.wear.compass.R
 import kotlin.platform.platformStatic
@@ -18,18 +22,35 @@ import kotlin.platform.platformStatic
 
 public class SelectPlaceFragment : Fragment() {
 
+    private var mGoogleApiClient: GoogleApiClient? = null
+
     private val startPlacePickerBtn: Button by bindView(R.id.startPlacePickerBtn)
     private val placeName: TextView by bindView(R.id.placeName)
     private val phoneNumber: TextView by bindView(R.id.phoneNumber)
     private val webSiteUrl: TextView by bindView(R.id.webSiteUrl)
+    private val autocompletePlaces: AutoCompleteTextView by bindView(R.id.autocomplete_places)
 
     companion object {
 
+        private val TAG: String = SelectPlaceFragment.javaClass.getSimpleName()
         private val PLACE_PICKER_REQUEST: Int = 1000
 
         platformStatic fun newInstance() : SelectPlaceFragment {
             return SelectPlaceFragment()
         }
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mGoogleApiClient = GoogleApiClient.Builder(getActivity())
+                .enableAutoManage(getActivity(), 0, GoogleApiClient.OnConnectionFailedListener {
+                    result -> Log.e(TAG, "GoogleApiClient error " + result.getErrorCode())
+                })
+                .addApi(Places.GEO_DATA_API)
+                .build()
+
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
