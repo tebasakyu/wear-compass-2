7e32621842bbfa036abcc52f9016fb3eb791cf71
s-makotoy <tebasakyu@yahoo.co.jp>
2015-09-10 21:36:03 +0900

    setting enable ambient

--- wear/src/main/kotlin/com/tebasaki/yu/wear/compass/activity/MainActivity.kt
+++ wear/src/main/kotlin/com/tebasaki/yu/wear/compass/activity/MainActivity.kt
@@ -3,29 +3,33 @@ package com.tebasaki.yu.wear.compass.activity
 import android.location.Location
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
+import android.support.wearable.activity.WearableActivity
 import android.support.wearable.view.WatchViewStub
-import android.util.Log
 import android.widget.TextView
 import com.tebasaki.yu.wear.compass.R
 import com.tebasaki.yu.wear.compass.fragment.DetectLocationFragment
 
-public class MainActivity : FragmentActivity(),
+public class MainActivity : WearableActivity(),
                             DetectLocationFragment.OnLocationListener {
 
     private var mTextView: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super<FragmentActivity>.onCreate(savedInstanceState)
+        super<WearableActivity>.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // supports ambient mode
+        setAmbientEnabled()
+
         val stub = findViewById(R.id.watch_view_stub) as WatchViewStub
         stub.setOnLayoutInflatedListener(object : WatchViewStub.OnLayoutInflatedListener {
             override fun onLayoutInflated(stub: WatchViewStub) {
                 mTextView = stub.findViewById(R.id.text) as TextView
                 mTextView?.setText("てすと")
 
-                getSupportFragmentManager().beginTransaction()
-                        .add(R.id.fl_container, DetectLocationFragment.newInstance())
-                        .commit()
+                getFragmentManager().beginTransaction()
+                    .add(R.id.fl_container, DetectLocationFragment.newInstance())
+                    .commit()
+
             }
         })
     }
