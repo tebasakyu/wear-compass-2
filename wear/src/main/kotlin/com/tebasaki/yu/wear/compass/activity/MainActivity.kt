3ebdbc8a00e38d113dde65a091e2904b8a1078bd
s-makotoy <tebasakyu@yahoo.co.jp>
2015-10-07 20:59:34 +0900

    Display to destination, No debug yet.

--- wear/src/main/kotlin/com/tebasaki/yu/wear/compass/activity/MainActivity.kt
+++ wear/src/main/kotlin/com/tebasaki/yu/wear/compass/activity/MainActivity.kt
@@ -4,7 +4,9 @@ import android.location.Location
 import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
 import android.support.wearable.view.WatchViewStub
-import android.widget.TextView
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
+import android.widget.ImageView
 import com.tebasaki.yu.wear.compass.R
 import com.tebasaki.yu.wear.compass.fragment.DetectCompassFragment
 import com.tebasaki.yu.wear.compass.fragment.DetectLocationFragment
@@ -14,8 +16,10 @@ public class MainActivity : WearableActivity(),
                             DetectCompassFragment.OnCompassListener {
 
     private var mCanUpdateViews: Boolean = false
+    private var mFromAzimuth: Float = 0f
+    private var mToAzimuth: Float = 0f
 
-    private var mTextView: TextView? = null
+    private var mImageView: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,27 +30,34 @@ public class MainActivity : WearableActivity(),
         val stub = findViewById(R.id.watch_view_stub) as WatchViewStub
         stub.setOnLayoutInflatedListener(object : WatchViewStub.OnLayoutInflatedListener {
             override fun onLayoutInflated(stub: WatchViewStub) {
-                mTextView = stub.findViewById(R.id.text) as TextView
+                mImageView = stub.findViewById(R.id.img) as ImageView
 
                 fragmentManager.beginTransaction()
                         .add(R.id.fl_container, DetectLocationFragment.newInstance())
                         .add(R.id.fl_container, DetectCompassFragment.newInstance())
                         .commit()
-
             }
         })
     }
 
     override fun onLocationChanged(location: Location) {
         if (mCanUpdateViews) {
-            mTextView?.text = "てすと:" + location.latitude + "," + location.longitude
+
+            // debug code 中目黒駅
+            val locationTo: Location = Location("")
+            locationTo.latitude = 35.6442877
+            locationTo.longitude = 139.6990956
+
+            val bearing: Float = location.bearingTo(locationTo)
+            displayToDestination(if (0 < bearing) { 360 - bearing } else { bearing * (-1)})
         }
     }
 
 
     override fun onCompassChanged(fromAzimuth: Float, toAzimuth: Float) {
         if (mCanUpdateViews) {
-
+            mFromAzimuth = fromAzimuth
+            mToAzimuth = toAzimuth
         }
     }
 
@@ -59,5 +70,20 @@ public class MainActivity : WearableActivity(),
     override fun onExitAmbient() {
         super.onExitAmbient()
         mCanUpdateViews = false
+    }
+
+
+    private fun displayToDestination(bearingTo: Float) {
+
+        val rotateFrom = bearingTo - mFromAzimuth
+        val rotateTo = bearingTo - mToAzimuth
+
+        val anim: Animation = RotateAnimation(-rotateFrom, -rotateTo,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+        anim.duration = 300
+        anim.repeatCount = 0
+        anim.fillAfter = true
+
+        mImageView?.startAnimation(anim)
     }
 }
